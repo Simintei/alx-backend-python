83092e58a3f3c15fb1fcd6893b0ef7edecf2a36c
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-09-20 14:04:26 +0000

    updated urls.py

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # <-- include your chats API
+    path('api/', include('chats.urls')),  # <-- include your app URLs
 ]
+
 
