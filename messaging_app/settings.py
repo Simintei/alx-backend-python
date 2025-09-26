404a181a5b08e7933bac9cea97f1f4fae0ba8a71
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-09-26 23:23:28 +0300

    Update settings.py

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -46,10 +46,13 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'chats.permissions.IsParticipantOfConversation',  # our custom permission globally
+    ),
 }
     
 MIDDLEWARE = [
