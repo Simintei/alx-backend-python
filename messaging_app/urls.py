ddfe676f048c236d8bd4b276925d38b6c6b043e6
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-09-26 23:03:38 +0300

    Update urls.py

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from messaging_app.chats.auth import token_obtain_pair, token_refresh
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # include our nested routes
-]
 
+    # JWT endpoints
+    path('api/token/', token_obtain_pair, name='token_obtain_pair'),
+    path('api/token/refresh/', token_refresh, name='token_refresh'),
+
+    # Include chats app URLs
+    path('api/chats/', include('messaging_app.chats.urls')),
+]
 
 
