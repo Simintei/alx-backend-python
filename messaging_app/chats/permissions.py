ed83647a42a55d882f42ef4c68af9a5bf3a8124e
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-09-26 22:52:46 +0300

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,9 @@
-
 from rest_framework.permissions import BasePermission
 
 class IsOwner(BasePermission):
+    """
+    Allow only owners of an object to view or edit it.
+    Assumes the model has a `user` field.
+    """
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
