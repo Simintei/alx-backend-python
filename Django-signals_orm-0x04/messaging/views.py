6a09f51f25397ad89cd3114fd540f2e57f38aa1c
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-10-04 14:12:04 +0300

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,1 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+
+@login_required
+def delete_user(request):
+    user = request.user
+    logout(request)  # Log out before deletion
+    user.delete()    # This triggers post_delete signal
+    return redirect("home")  # Redirect to homepage or goodbye page
 
