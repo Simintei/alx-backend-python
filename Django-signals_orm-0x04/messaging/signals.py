9b7270917d192b3225f5856f115232d87ad9a27e
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-10-04 14:14:32 +0300

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,9 +1,18 @@
-from django.db.models.signals import pre_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Message, MessageHistory, Notification
 
-#  Already implemented earlier for logging edits
+#  Create notification when a new message is created
+@receiver(post_save, sender=Message)
+def create_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance
+        )
+
+#  Log old content before updating a message
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
     if instance.pk:
@@ -16,20 +25,14 @@ def log_message_edit(sender, instance, **kwargs):
             MessageHistory.objects.create(
                 message=instance,
                 old_content=old_message.content,
-                edited_by=instance.edited_by  # Save who edited
+                edited_by=instance.edited_by
             )
             instance.edited = True
-
 
 #  Cleanup after user deletion
 @receiver(post_delete, sender=User)
 def cleanup_user_related_data(sender, instance, **kwargs):
-    # Delete messages sent or received by the user
     Message.objects.filter(sender=instance).delete()
     Message.objects.filter(receiver=instance).delete()
-
-    # Delete notifications for this user
     Notification.objects.filter(user=instance).delete()
-
-    # Delete message histories where user was the editor
     MessageHistory.objects.filter(edited_by=instance).delete()
