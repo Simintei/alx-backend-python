8ac306ff55477d2f4a3919a8c06e837452f750a6
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-10-04 14:11:01 +0300

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, related_name="sent_messages", on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, related_name="received_messages", on_delete=models.CASCADE)
+    sender = models.ForeignKey(
+        User, related_name="sent_messages", on_delete=models.CASCADE
+    )
+    receiver = models.ForeignKey(
+        User, related_name="received_messages", on_delete=models.CASCADE
+    )
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    edited = models.BooleanField(default=False)  # Track if message was edited
-    edited_by = models.ForeignKey(   # track who edited
+    edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(
         User,
         related_name="edited_messages",
         on_delete=models.SET_NULL,
@@ -22,7 +26,7 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, related_name="history", on_delete=models.CASCADE)
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
-    edited_by = models.ForeignKey(   #  Track who made the edit
+    edited_by = models.ForeignKey(
         User,
         related_name="message_edits",
         on_delete=models.SET_NULL,
@@ -32,3 +36,12 @@ class MessageHistory(models.Model):
 
     def __str__(self):
         return f"History of message {self.message.id} at {self.edited_at}"
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, related_name="notifications", on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, related_name="notifications", on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.username} - {self.message}"
