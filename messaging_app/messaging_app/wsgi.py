fac1501c0ca32f301f74e8a3c3f19016cc2cd538
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-09 21:19:44 +0300

    Update wsgi.py

--- messaging_app/messaging_app/wsgi.py
+++ messaging_app/messaging_app/wsgi.py
@@ -1,6 +1,12 @@
 import os
+import sys
+
+path = '/home/MaryKaranja/alx-backend-python/messaging_app'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'messaging_app.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
-
 application = get_wsgi_application()
+
